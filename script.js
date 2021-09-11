8a7079a2239c14ecb7747056e7b93ea8eee7cc67
inmarluna <inmarluna@Inmars-MacBook-Pro.local>
2021-09-10 20:09:10 -0500

    added variables and prompts

--- script.js
+++ script.js
@@ -1,30 +1,56 @@
 // Assignment code here
 
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
   // variables
-  var upper = "65, 90";
-  var lower = "97, 122";
-  var number = "48, 57";
-  var special = "33, 47";
+  var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lower = "abcdefghijklmnopqrstuvwxyz";
+  var number = "0123456789";
+  var special = "#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+
+  var newPassword = "";
   // function to generate a random password
   var generatePassword = function() {
+    var result = "";
     alert("Please select from the following password criteria:");
-  
-  var newPassword = "";
 
-  // prompt user to select password length
-  var passLength = parseInt(prompt("Choose a password between 8 and 128 characters."));
-
+  // prompt user to select password length and confirm password characters
+  var passLength = prompt ("Password must be between 8 and 128 characters. Please select a number.");
   if (passLength < 8 || passLength > 128) {
       alert("Please select a password between 8 and 128 charcters long! Please try again.");
-      return(generatePassword);
+      return generatePassword ();
   }
   else {
+    let lowerCase = confirm ("Select OK to include lowercase characters?");
+    if (lowerCase === lower) {
+      newPassword += lower
+    }
+    let upperCase = confirm ("Select OK to include uppercase characters?");
+    if (upperCase === upper) {
+      newPassword += upper
+    }
+    let numbers = confirm ("Select OK to include numbers?");
+    if (numbers === number) {
+      newPassword += number
+    }
+    let specialChars = confirm ("Select OK to include special characters?");
+    if (specialChars === special) {
+      newPassword += special
+    }
 
+    // if user did not select a valid option
+    if (upper === false && lower === false && number === false && special === false) {
+      alert("You must select at least one character option!");
+    }
+    // loop to select a random password 
+    for (var i = 0; i < passLength; i++) {
+      result += newPassword.charAt(Math.floor(Math.random() * passLength.length));
+    }
+    return result;
+
+    }
   }
-
-
-}
-// Get references to the #generate element
 
 
 // Write password to the #password input
@@ -37,4 +63,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generatePassword.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword);
